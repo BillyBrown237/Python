c1121f44766c4824d111544648111d393ff970d7
billybrown237 <kano.billybrown@gmail.com>
2025-02-13 23:08:07 +0100

    input -outputs done

---
+++ input_output.py
@@ -0,0 +1,14 @@
+# input() function used to collect input from the user e.g
+# it is possible to collect multiple inputs using different input functions because they are executed sequentially
+email = input('Please enter your email address ')
+print(email)
+
+# print function that returns data to the user
+# it has reserved keywords that can be passed as params 
+#  `object` values that are printed on screen
+# `sep` which defines how printed objects are separated
+# `end` which defines what get's printed
+# `file` which defines where values get printed to
+# `flush` which removes the data from computer temporary storage to permanent storage
+
+# NB: if using inputs to collect numbers for arithmetic operations convert the input data to the appropriate type for the operation
