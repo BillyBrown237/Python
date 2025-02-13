1c7e9386d6ff3a25a615e56190a060991593a96c
billybrown237 <kano.billybrown@gmail.com>
2025-02-13 22:38:17 +0100

    completed strings

---
+++ strings.py
@@ -0,0 +1,22 @@
+# strings are considered as a collection of character
+name = 'john'
+name[0]
+print(len(name))
+
+a = 'hello'
+print(a)
+
+b = "hello"
+print(b)
+
+single_line = 'This is a single line'
+print(single_line)
+
+multi_line = 'this is a multi' \
+' line string right'
+print(multi_line)
+
+con_a = 'concatenate'
+con_b = ' string'
+print(con_a + con_b)
+
